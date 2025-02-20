6bd24117daecfe9b2c126d15b22282e79139e662
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-20 12:50:27 -0300

    feat(routes/books): create all routes for books. CRUD (Create, Read, Update, Delete)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,11 @@ const AuthorController = require('./app/controllers/AuthorController');
 
 const router = Router();
 
+router.post('/books', BookController.store);
 router.get('/books', BookController.index);
+router.get('/books/:id', BookController.show);
+router.put('/books/:id', BookController.update);
+router.delete('/books/:id', BookController.delete);
 
 router.get('/authors', AuthorController.index);
 
