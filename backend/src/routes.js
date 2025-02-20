db0f2896154c2372b28fa7aad16c0c850cf441d2
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-20 12:41:01 -0300

    feat(routes/authors): add a new route of authors. route to get all the authors in the application

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 
-const BookController = require('./app/controllers/BookController')
+const BookController = require('./app/controllers/BookController');
+const AuthorController = require('./app/controllers/AuthorController');
 
 const router = Router();
 
 router.get('/books', BookController.index);
+
+router.get('/authors', AuthorController.index);
 
 module.exports = router;
