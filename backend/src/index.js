01c52954b7c9e84b5df99e2c43d1b0c6a18ee4bf
viniciussnitram <vinicius.martins.vsm@outlook.com>
2025-02-20 12:37:19 -0300

    refactor(index.js): refactor the index of server to be more organized. separating routes from index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
+
+const routes = require('./routes');
 
 const app = express();
+app.use(routes);
 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
-});
-
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3000, () => console.log('Server started at http://localhost:3000'))
